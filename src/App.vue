b07bc5da97ba9527c19bafe183d6bc5416098c2a
dirk teucher <bluemagic@hotmail.com>
2018-04-14 15:15:10 +0100

    setting some default styles
    Eric meyer reset knowledge is useful

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,21 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 20px;
+  color: #fff;
+  height: 100%;
+}
+
+html, body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  background-color: #141518;
+}
+h1, h2 {
+  font-weight: normal;
+}
+
+a {
+  color: #42b983;
 }
 </style>
